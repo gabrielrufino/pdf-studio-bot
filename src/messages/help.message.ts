d492ace4cc009bbe87813e467c7d9526d73d911b
Gabriel Rufino <contato@gabrielrufino.com>
2026-05-11 18:20:33 -0300

    refactor(help): move command sets to static properties and add fallback

--- src/messages/help.message.ts
+++ src/messages/help.message.ts
@@ -3,31 +3,34 @@ import { InlineKeyboard } from 'grammy'
 import { CommandEnum } from '../enums/command.enum'
 
 export class HelpMessage {
+  private static readonly OPERATIONS = new Set<string>([
+    CommandEnum.Download,
+    CommandEnum.Join,
+    CommandEnum.PdfToImages,
+    CommandEnum.PutPassword,
+    CommandEnum.Split,
+    CommandEnum.Summary,
+  ])
+
+  private static readonly INFORMATION = new Set<string>([
+    CommandEnum.Pro,
+    CommandEnum.Feedback,
+    CommandEnum.Version,
+    CommandEnum.Help,
+  ])
+
   constructor(private readonly handlers: BaseHandler[]) {}
 
   public build() {
     const keyboard = new InlineKeyboard()
 
-    const operations = new Set<string>([
-      CommandEnum.Download,
-      CommandEnum.Join,
-      CommandEnum.PdfToImages,
-      CommandEnum.PutPassword,
-      CommandEnum.Split,
-      CommandEnum.Summary,
-    ])
+    const operationHandlers = this.handlers.filter(h => HelpMessage.OPERATIONS.has(h.command))
+    const informationHandlers = this.handlers.filter(h => HelpMessage.INFORMATION.has(h.command))
+    const otherHandlers = this.handlers.filter(
+      h => !HelpMessage.OPERATIONS.has(h.command) && !HelpMessage.INFORMATION.has(h.command),
+    )
 
-    const information = new Set<string>([
-      CommandEnum.Pro,
-      CommandEnum.Feedback,
-      CommandEnum.Version,
-      CommandEnum.Help,
-    ])
-
-    const operationHandlers = this.handlers.filter(h => operations.has(h.command))
-    const informationHandlers = this.handlers.filter(h => information.has(h.command))
-
-    const allHandlers = [...operationHandlers, ...informationHandlers]
+    const allHandlers = [...operationHandlers, ...informationHandlers, ...otherHandlers]
 
     allHandlers.forEach((h) => {
       keyboard.text(h.description, h.command).row()
